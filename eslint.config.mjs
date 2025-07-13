ddd9391e5ffcd9d0425aad3f863f2733f4e2608a
Valentin Peltier <valentin.peltier@proton.me>
2025-07-13 22:19:55 +0200

    🔧 (eslint): fix config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,22 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { defineConfig } from "eslint/config";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default defineConfig([
+    ...new FlatCompat({
+        baseDirectory: import.meta.dirname,
+    }).extends("next/core-web-vitals", "next/typescript"),
+    {
+        files: [
+            "**/*.ts",
+            "**/*.tsx",
+            "**/*.js",
+            "**/*.mjs",
+        ],
+        rules: {
+            "indent": "warn",
+            "semi": "error",
+            "quotes": "warn",
+            "@typescript-eslint/no-unused-vars": "warn",
+        },
+    },
+]);
