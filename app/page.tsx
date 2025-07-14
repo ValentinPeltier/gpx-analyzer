7b5cfdacbde4c2dde39d73f5ef99cf9d19992a96
Valentin Peltier <valentin.peltier@proton.me>
2025-07-14 02:14:14 +0200

    ✨ (Menubar): add component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,60 @@
+"use client";
+
+import { useState } from "react";
+import { Menubar } from "@/components/menubar";
+import GpxFile from "@/utils/GpxFile";
+
 export default function Home() {
+    const [files, setFiles] = useState<GpxFile[]>([
+        new GpxFile("test1.gpx", ""),
+        new GpxFile("test2.gpx", ""),
+    ]);
+    const recentFileNames = [
+        "recent1.gpx",
+    ];
+    const [currentFileName, setCurrentFileName] = useState<string>(files[0].getName());
+
+    const openFile = (file: GpxFile) => {
+        // If the file is not already open
+        if (files.findIndex((f) => f.getName() === file.getName()) === -1) {
+            // Add the file
+            setFiles(files.concat([file]));
+        }
+
+        // Switch to the new tab
+        setCurrentFileName(file.getName());
+    };
+
+    const closeFile = (fileName: string) => {
+        if (fileName !== undefined) {
+            const fileIndex = files.findIndex((file) => file.getName() === fileName);
+            if (fileIndex !== -1) {
+                // Remove the file
+                setFiles(files.toSpliced(fileIndex, 1));
+
+                // Switch to the previous tab
+                if (fileIndex - 1 >= 0) {
+                    setCurrentFileName(files[fileIndex - 1].getName());
+                }
+            }
+        }
+    };
+
     return (
         <div>
-            <main></main>
+            <header>
+                <Menubar
+                    recentFiles={recentFileNames}
+                    onFileOpen={openFile}
+                    onFileClose={() => {
+                        if (currentFileName !== null) {
+                            closeFile(currentFileName);
+                        }
+                    }}
+                />
+            </header>
+            <main>
+            </main>
         </div>
     );
 }
---
+++ components/menubar.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useRef } from "react";
+import { toast } from "sonner";
+import {
+    Menubar as MenubarPrimitive,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarSeparator,
+    MenubarSub,
+    MenubarSubContent,
+    MenubarSubTrigger,
+    MenubarTrigger,
+} from "@/components/ui/menubar";
+import { Input } from "@/components/ui/input";
+import GpxFile from "@/utils/GpxFile";
+
+export const Menubar = ({
+    recentFiles: recentFileNames,
+    onFileOpen,
+    onFileClose,
+}: {
+    recentFiles: string[],
+    onFileOpen: (file: GpxFile) => void,
+    onFileClose: () => void,
+}) => {
+    const fileRef = useRef<HTMLInputElement>(null);
+
+    return <>
+        <Input
+            type="file"
+            multiple={true}
+            accept="application/gpx+xml"
+            className="hidden"
+            ref={fileRef}
+            onChange={async (e) => {
+                const fileList = e.target.files;
+
+                if (fileList !== null) {
+                    // Open all files
+                    for (const file of fileList) {
+                        let gpxFile;
+                        try {
+                            gpxFile = new GpxFile(file.name, await file.text());
+                        }
+                        catch (error) { // eslint-disable-line @typescript-eslint/no-unused-vars
+                            toast(`Impossible d'ouvrir le fichier ${file.name}`, {
+                                "className": "text-red-500",
+                            });
+                            continue;
+                        }
+                        onFileOpen(gpxFile);
+                    }
+                }
+            }}
+        />
+        <MenubarPrimitive>
+            <MenubarMenu>
+                <MenubarTrigger>Fichier</MenubarTrigger>
+                <MenubarContent>
+                    <MenubarItem onSelect={() => fileRef.current?.click()}>Charger un fichier...</MenubarItem>
+
+                    <MenubarSub>
+                        <MenubarSubTrigger>Ouvrir un fichier récent</MenubarSubTrigger>
+                        <MenubarSubContent>
+                            {recentFileNames.map((recentFileName, index) => (
+                                <MenubarItem key={index}>{recentFileName}</MenubarItem>
+                            ))}
+                        </MenubarSubContent>
+                    </MenubarSub>
+
+                    <MenubarSeparator />
+
+                    <MenubarItem onSelect={() => onFileClose()}>Fermer le fichier</MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+        </MenubarPrimitive>
+    </>;
+};
